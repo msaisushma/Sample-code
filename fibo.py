e23beb72598e6898e273c251bb386969b0ff1afd
smsaisushma <smsaisushma@gmail.com>
2014-04-01 00:48:11 +0530

    Improved Code

--- fibo.py
+++ fibo.py
@@ -1,10 +1,9 @@
 def fib():
-	a,b,c,rl,n=-1,1,0,[],int(raw_input("Enter position="))
+	a,b,c,n=-1,1,0,int(raw_input("Enter position="))
 	for i in range(n):
 		c=a+b
 		b,a=c,b
-		rl.append(c)
-	print rl[-1]
+	print c
 
 if __name__=='__main__':
 	fib()
